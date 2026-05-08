873f8f3f44b334ad0ca30f7e1db0de09774d44f5
root <root@vmi3251968.contaboserver.net>
2026-05-08 17:33:10 +0200

    Configuración de ALLOWED_HOSTS para DuckDNS y limpieza de migraciones post-merge

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ SITE_URL = os.getenv("SITE_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.238.234.86', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['178.238.234.86', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['178.238.234.86', 'guillermoai.duckdns.org', 'www.guillermoai.duckdns.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
